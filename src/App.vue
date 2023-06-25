40b26dea1c01a23932b3189ecef857922120c644
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2023-06-25 02:55:31 -0300

    Set up Quasar

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100vw;
   height: 100vh;
-}
-
-body, p, h1, h2, h3 {
-  padding: 0;
-  margin: 0;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import OpenLayersMap from 'vue3-openlayers';
 import 'vue3-openlayers/dist/vue3-openlayers.css';
 
 import { createApp } from 'vue';
+import { Quasar } from 'quasar';
 import router from './router';
 import App from './App.vue';
+import quasarUserOptions from './quasar-user-options';
 
-createApp(App)
+createApp(App).use(Quasar, quasarUserOptions)
   .use(router)
   .use(OpenLayersMap)
   .mount('#app');
