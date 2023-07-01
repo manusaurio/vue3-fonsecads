21074cef2debf7662f8aa4bea36465c354f33597
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2023-06-30 23:51:27 -0300

    Implement top navigation bar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-toolbar class="text-primary shadow-1">
+    <q-btn flat round dense icon="arrow_back" @click="router.back()" />
+    <q-toolbar-title>
+      {{ title }}
+    </q-toolbar-title>
+  </q-toolbar>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+defineProps({
+  title: String,
+});
+</script>
