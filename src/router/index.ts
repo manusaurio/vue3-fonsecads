e287fc3366fa55542935a5849a0854da8c89c602
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2023-07-10 03:40:59 -0300

    Set up routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import store from '@/store';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/map',
+    path: '/',
+    redirect: { name: 'map' },
+  },
+  {
+    path: '/map/:coord?',
     name: 'map',
-    component: () => import(/* webpackChunkName: "map" */ '../views/MapView.vue'),
+    component: () => import('../views/MapView.vue'),
   },
   {
     path: '/view',
@@ -15,11 +20,20 @@ const routes: Array<RouteRecordRaw> = [
     path: '/compose/locate',
     name: 'locate',
     component: () => import('../views/PlacementView.vue'),
+    beforeEnter: (to) => {
+      try {
+        BigInt(`0x${to.query.msg}`);
+        return true;
+      } catch (e) {
+        return { name: 'map' };
+      }
+    },
   },
   {
     path: '/compose',
     name: 'compose',
     component: () => import('../views/ComposeView.vue'),
+    beforeEnter: () => !(store.mapMeta.getLastPoint() === undefined) || { name: 'map' },
   },
 ];
 
