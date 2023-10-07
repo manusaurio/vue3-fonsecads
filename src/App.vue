3842b4df07e6147e6fc5a57f85dbd67586207c09
Manuel Ignacio Pérez Alcolea <perezalcoleam@gmail.com>
2023-10-07 00:21:55 -0300

    Include loading feedback at outermost App layer

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <LoadingFeedback ref="loadingFeedback"></LoadingFeedback>
   <router-view/>
 </template>
 
@@ -8,3 +9,22 @@
   height: 100vh;
 }
 </style>
+
+<script setup lang='ts'>
+import { ref, provide } from 'vue';
+import eventBus from '@/event-bus';
+
+const loadingFeedback = ref(null);
+
+eventBus.on('loading', (v: boolean) => {
+  if (v) loadingFeedback.value.show();
+  else loadingFeedback.value.hide();
+});
+
+const cambio = (o: string | undefined) => {
+  if (o === undefined) loadingFeedback.value.hide();
+  else loadingFeedback.value.show();
+};
+
+provide('feedback', loadingFeedback);
+</script>
